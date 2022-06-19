95f2de890a31c052be79896ffabaa9279d1a4093
Subhrajyoti2608 <86191223+Subhrajyoti2608@users.noreply.github.com>
2022-06-19 11:45:58 +0530

    Add files via upload

--- dice.py
+++ dice.py
@@ -2,7 +2,43 @@ import random
 dice = True,
 
 while dice:
-    print(random.randint(1,6))
+    no=random.randint(1,6)
+    if no == 1 :
+        print("[-----]")
+        print("[-----]")
+        print("[  0  ]")
+        print("[-----]")
+        print("[-----]")
+    if no == 2 :
+        print("[-----]")
+        print("[-----]")
+        print("[0   0]")
+        print("[-----]")
+        print("[-----]")
+    if no == 3 :
+        print("[-----]")
+        print("[-----]")
+        print("[ 000 ]")
+        print("[-----]")
+        print("[-----]")
+    if no == 4 :
+        print("[-----]")
+        print("[0   0]")
+        print("[     ]")
+        print("[0   0]")
+        print("[-----]")
+    if no == 5 :
+        print("[-----]")
+        print("[0   0]")
+        print("[  0  ]")
+        print("[0   0]")
+        print("[-----]")
+    if no == 6 :
+        print("[-----]")
+        print("[0   0]")
+        print("[0   0]")
+        print("[0   0]")
+        print("[-----]")
     another_roll = input("Want to roll the dice again? (y/n): ")
     if another_roll.lower() == "y":
         continue
